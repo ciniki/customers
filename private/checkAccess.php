5659c5288c8ca60abf78c36483b679df9cb10650
Andrew Rivett <veggiefrog@gmail.com>
2012-05-26 23:03:00 +0000

    Removed features module, cleaned up checkAccess and added business checkModuleAccess function

--- private/checkAccess.php
+++ private/checkAccess.php
@@ -19,26 +19,18 @@
 //
 function ciniki_customers_checkAccess($ciniki, $business_id, $method, $customer_id) {
 	//
-	// All customers module functions require authentication, so users 
-	// are authenicated before they reach this function
+	// Check if the business is active and the module is enabled
 	//
-
-	//
-	// Check if the module is turned on for the business
-	// Check the business is active
-	//
-	$strsql = "SELECT ruleset FROM ciniki_businesses, ciniki_business_modules "
-		. "WHERE ciniki_businesses.id = '" . ciniki_core_dbQuote($ciniki, $business_id) . "' "
-		. "AND ciniki_businesses.status = 1 "														// Business is active
-		. "AND ciniki_businesses.id = ciniki_business_modules.business_id "
-		. "AND ciniki_business_modules.package = 'ciniki' "
-		. "AND ciniki_business_modules.module = 'customers' "
-		. "";
-	require_once($ciniki['config']['core']['modules_dir'] . '/core/private/dbHashQuery.php');
-	$rc = ciniki_core_dbHashQuery($ciniki, $strsql, 'businesses', 'module');
+	require_once($ciniki['config']['core']['modules_dir'] . '/businesses/private/checkModuleAccess.php');
+	$rc = ciniki_businesses_checkModuleAccess($ciniki, $business_id, 'ciniki', 'customers');
 	if( $rc['stat'] != 'ok' ) {
 		return $rc;
 	}
+
+	if( !isset($rc['ruleset']) ) {
+		return array('stat'=>'fail', 'err'=>array('pkg'=>'ciniki', 'code'=>'582', 'msg'=>'No permissions granted'));
+	}
+	$modules = $rc['modules'];
 
 	//
 	// Sysadmins are allowed full access
