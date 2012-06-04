086b11a846db558356a81108bc4d2ecab8fa4dce
Andrew Rivett <veggiefrog@gmail.com>
2012-06-04 10:14:43 -0400

    Added recent list to customer module

---
+++ public/recent.php
@@ -0,0 +1,57 @@
+<?php
+//
+// Description
+// -----------
+// Return the list of customer who have been recently updated
+//
+// Arguments
+// ---------
+// user_id: 		The user making the request
+// search_str:		The search string provided by the user.
+// 
+// Returns
+// -------
+//
+function ciniki_customers_recent($ciniki) {
+    //  
+    // Find all the required and optional arguments
+    //  
+    require_once($ciniki['config']['core']['modules_dir'] . '/core/private/prepareArgs.php');
+    $rc = ciniki_core_prepareArgs($ciniki, 'no', array(
+        'business_id'=>array('required'=>'yes', 'blank'=>'no', 'errmsg'=>'No business specified'), 
+        'limit'=>array('required'=>'no', 'blank'=>'no', 'errmsg'=>'No limit specified'), 
+        )); 
+    if( $rc['stat'] != 'ok' ) { 
+        return $rc;
+    }   
+    $args = $rc['args'];
+    
+    //  
+    // Make sure this module is activated, and
+    // check permission to run this function for this business
+    //  
+    require_once($ciniki['config']['core']['modules_dir'] . '/customers/private/checkAccess.php');
+    $rc = ciniki_customers_checkAccess($ciniki, $args['business_id'], 'ciniki.customers.recent', 0); 
+    if( $rc['stat'] != 'ok' ) { 
+        return $rc;
+    }   
+
+	//
+	// Get the number of customers in each status for the business, 
+	// if no rows found, then return empty array
+	//
+	$strsql = "SELECT id, CONCAT_WS(' ', first, last) AS name, status "
+		. "FROM ciniki_customers "
+		. "WHERE business_id = '" . ciniki_core_dbQuote($ciniki, $args['business_id']) . "' "
+		. "AND status = 1 "
+		. "ORDER BY last_updated DESC, last, first DESC ";
+	if( isset($args['limit']) && is_numeric($args['limit']) && $args['limit'] > 0 ) {
+		$strsql .= "LIMIT " . ciniki_core_dbQuote($ciniki, $args['limit']) . " ";	// is_numeric verified
+	} else {
+		$strsql .= "LIMIT 25 ";
+	}
+
+	require_once($ciniki['config']['core']['modules_dir'] . '/core/private/dbRspQuery.php');
+	return ciniki_core_dbRspQuery($ciniki, $strsql, 'customers', 'customers', 'customer', array('stat'=>'ok', 'customers'=>array()));
+}
+?>
