4e87e05ad754a97a7c92581d26255924029da275
Andrew Rivett <veggiefrog@gmail.com>
2012-07-02 02:12:51 +0000

    Update web delivery to allow customers to change password

---
+++ web/changePassword.php
@@ -0,0 +1,102 @@
+<?php
+//
+// Description
+// -----------
+// This function will change the users password, providing their own one is correct.
+//
+// Arguments
+// ---------
+// api_key:
+// auth_token:
+//
+// temppassword: 	The temporary password for the user.  
+//
+// newpassword: 	The new password for the user.
+//
+// Returns
+// -------
+// <stat='ok' />
+//
+function ciniki_customers_web_changePassword($ciniki, $business_id, $oldpassword, $newpassword) {
+	//
+	// Find all the required and optional arguments
+	//
+	require_once($ciniki['config']['core']['modules_dir'] . '/core/private/dbQuote.php');
+
+	if( strlen($newpassword) < 8 ) {
+		return array('stat'=>'fail', 'err'=>array('pkg'=>'ciniki', 'code'=>'749', 'msg'=>'New password must be longer than 8 characters.'));
+	}
+
+	if( !isset($ciniki['session']['customer']['email']) ) {
+		return array('stat'=>'fail', 'err'=>array('pkg'=>'ciniki', 'code'=>'750', 'msg'=>'You must be signed in to change your password.'));
+	}
+
+	//
+	// Check temp password
+	// Must change password within 2 hours (7200 seconds)
+	$strsql = "SELECT id, email "
+		. "FROM ciniki_customer_emails "
+		. "WHERE business_id = '" . ciniki_core_dbQuote($ciniki, $business_id) . "' "
+		. "AND email = '" . ciniki_core_dbQuote($ciniki, $ciniki['session']['customer']['email']) . "' "
+		. "AND password = SHA1('" . ciniki_core_dbQuote($ciniki, $oldpassword) . "') "
+		. "";
+	require_once($ciniki['config']['core']['modules_dir'] . '/core/private/dbHashQuery.php');
+	$rc = ciniki_core_dbHashQuery($ciniki, $strsql, 'users', 'user');
+	if( $rc['stat'] != 'ok' ) {
+		return $rc;
+	}
+	if( !isset($rc['user']) || !is_array($rc['user']) ) {
+		return array('stat'=>'fail', 'err'=>array('pkg'=>'ciniki', 'code'=>'751', 'msg'=>'Unable to update password.'));
+	}
+	$user = $rc['user'];
+
+	//
+	// Perform an extra check to make sure only 1 row was found, other return error
+	//
+	if( $rc['num_rows'] != 1 ) {
+		return array('stat'=>'fail', 'err'=>array('pkg'=>'ciniki', 'code'=>'752', 'msg'=>'Invalid temporary password'));
+	}
+
+	//
+	// Turn off autocommit
+	//
+	require_once($ciniki['config']['core']['modules_dir'] . '/core/private/dbTransactionStart.php');
+	require_once($ciniki['config']['core']['modules_dir'] . '/core/private/dbTransactionRollback.php');
+	require_once($ciniki['config']['core']['modules_dir'] . '/core/private/dbTransactionCommit.php');
+	$rc = ciniki_core_dbTransactionStart($ciniki, 'users');
+	if( $rc['stat'] != 'ok' ) {
+		return $rc;
+	}
+
+	//
+	// Update the password, but only if the temporary one matches
+	//
+	$strsql = "UPDATE ciniki_customer_emails SET password = SHA1('" . ciniki_core_dbQuote($ciniki, $newpassword) . "'), "
+		. "temp_password = '', "
+		. "last_updated = UTC_TIMESTAMP() "
+		. "WHERE id = '" . ciniki_core_dbQuote($ciniki, $user['id']) . "' "
+		. "AND password = SHA1('" . ciniki_core_dbQuote($ciniki, $oldpassword) . "') ";
+	require_once($ciniki['config']['core']['modules_dir'] . '/core/private/dbUpdate.php');
+	$rc = ciniki_core_dbUpdate(&$ciniki, $strsql, 'users');
+	if( $rc['stat'] != 'ok' ) {
+	error_log($strsql);
+		ciniki_core_dbTransactionRollback($ciniki, 'users');
+		return array('stat'=>'fail', 'err'=>array('pkg'=>'ciniki', 'code'=>'753', 'msg'=>'Unable to update password.'));
+	}
+
+	if( $rc['num_affected_rows'] < 1 ) {
+		ciniki_core_dbTransactionRollback($ciniki, 'users');
+		return array('stat'=>'fail', 'err'=>array('pkg'=>'ciniki', 'code'=>'754', 'msg'=>'Unable to change password.'));
+	}
+
+	//
+	// Commit all the changes to the database
+	//
+	$rc = ciniki_core_dbTransactionCommit($ciniki, 'users');
+	if( $rc['stat'] != 'ok' ) {
+		return array('stat'=>'fail', 'err'=>array('pkg'=>'ciniki', 'code'=>'755', 'msg'=>'Unable to update password.'));
+	}
+
+	return array('stat'=>'ok');
+}
+?>
