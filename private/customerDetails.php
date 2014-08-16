e39438f0d331497289b35654a2990fb80036bd63
Andrew Rivett <veggiefrog@gmail.com>
2014-08-16 01:28:28 -0400

    Added pricepoint to details

--- private/customerDetails.php
+++ private/customerDetails.php
@@ -57,7 +57,7 @@ function ciniki_customers__customerDetails($ciniki, $business_id, $customer_id, 
 		. "display_name, company, department, title, "
 		. "ciniki_customer_emails.id AS email_id, ciniki_customer_emails.email, "
 		. "IFNULL(DATE_FORMAT(birthdate, '" . ciniki_core_dbQuote($ciniki, $date_format) . "'), '') AS birthdate, "
-		. "notes "
+		. "pricepoint_id, notes "
 		. "FROM ciniki_customers "
 		. "LEFT JOIN ciniki_customer_emails ON (ciniki_customers.id = ciniki_customer_emails.customer_id) "
 		. "WHERE ciniki_customers.business_id = '" . ciniki_core_dbQuote($ciniki, $business_id) . "' "
@@ -65,7 +65,7 @@ function ciniki_customers__customerDetails($ciniki, $business_id, $customer_id, 
 	$rc = ciniki_core_dbHashQueryTree($ciniki, $strsql, 'ciniki.customers', array(
 		array('container'=>'customers', 'fname'=>'id', 'name'=>'customer',
 			'fields'=>array('id', 'eid', 'type', 'prefix', 'first', 'middle', 'last', 'suffix', 'display_name', 
-				'company', 'department', 'title', 
+				'company', 'department', 'title', 'pricepoint_id',
 				'notes', 'birthdate')),
 		array('container'=>'emails', 'fname'=>'email_id', 'name'=>'email',
 			'fields'=>array('id'=>'email_id', 'customer_id'=>'id', 'address'=>'email')),
