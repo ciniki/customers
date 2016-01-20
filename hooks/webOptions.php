9c3276a44f04a2c60e2853343ea7ab7556f2cf1b
Andrew Rivett <veggiefrog@gmail.com>
2016-01-20 17:06:41 -0500

    Added dealers map listing

---
+++ hooks/webOptions.php
@@ -0,0 +1,108 @@
+<?php
+//
+// Description
+// -----------
+// This function will return the list of options for the module that can be set for the website.
+//
+// Arguments
+// ---------
+// ciniki:
+// settings:		The web settings structure.
+// business_id:		The ID of the business to get options for.
+//
+// args:			The possible arguments for profiles
+//
+//
+// Returns
+// -------
+//
+function ciniki_customers_hooks_webOptions(&$ciniki, $business_id, $args) {
+
+	//
+	// Check to make sure the module is enabled
+	//
+	if( !isset($ciniki['business']['modules']['ciniki.customers']) ) {
+		return array('stat'=>'fail', 'err'=>array('pkg'=>'ciniki', 'code'=>'3042', 'msg'=>"I'm sorry, the page you requested does not exist."));
+	}
+
+	//
+	// Get the settings from the database
+	//
+	ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbDetailsQueryDash');
+	$rc = ciniki_core_dbDetailsQueryDash($ciniki, 'ciniki_web_settings', 'business_id', $business_id, 'ciniki.web', 'settings', 'page');
+	if( $rc['stat'] != 'ok' ) {
+		return $rc;
+	}
+	if( !isset($rc['settings']) ) {
+		$settings = array();
+	} else {
+		$settings = $rc['settings'];
+	}
+
+	$pages['ciniki.customers.dealers'] = array('name'=>'Dealers', 'options'=>array(
+        array('label'=>'Expand Short Name',
+            'setting'=>'page-dealers-locations-map-names', 
+            'type'=>'toggle',
+            'value'=>(isset($settings['page-dealers-locations-map-names'])?$settings['page-dealers-locations-map-names']:'no'),
+            'toggles'=>array(
+                array('value'=>'no', 'label'=>'No'),
+                array('value'=>'yes', 'label'=>'Yes'),
+                ),
+            ),
+        array('label'=>'Display Dealer Locations',
+            'setting'=>'page-dealers-locations-display', 
+            'type'=>'toggle',
+            'value'=>(isset($settings['page-dealers-locations-display'])?$settings['page-dealers-locations-display']:'no'),
+            'toggles'=>array(
+                array('value'=>'no', 'label'=>'No'),
+                array('value'=>'wordlist', 'label'=>'List'),
+                array('value'=>'wordcloud', 'label'=>'Cloud'),
+                ),
+            ),
+        array('label'=>'Listing Content',
+            'setting'=>'page-dealers-list-format', 
+            'type'=>'select',
+            'value'=>(isset($settings['page-dealers-list-format'])?$settings['page-dealers-list-format']:'no'),
+            'options'=>array(
+                array('value'=>'shortbio', 'label'=>'Short Bio'),
+                array('value'=>'shortbio-blank-addressesnl-phones-emails-links', 'label'=>'Short Bio, Addresses, Phones, Emails, Links'),
+                array('value'=>'addressesnl-blank-shortbio-phones-emails-links', 'label'=>'Addresses, Short Bio, Phones, Emails, Links'),
+                ),
+            ),
+        ));
+
+	$pages['ciniki.customers.distributors'] = array('name'=>'Distributors', 'options'=>array(
+        array('label'=>'Expand Short Name',
+            'setting'=>'page-distributors-locations-map-names', 
+            'type'=>'toggle',
+            'value'=>(isset($settings['page-distributors-locations-map-names'])?$settings['page-distributors-locations-map-names']:'no'),
+            'toggles'=>array(
+                array('value'=>'no', 'label'=>'No'),
+                array('value'=>'yes', 'label'=>'Yes'),
+                ),
+            ),
+        array('label'=>'Display Dealer Locations',
+            'setting'=>'page-distributors-locations-display', 
+            'type'=>'toggle',
+            'value'=>(isset($settings['page-distributors-locations-display'])?$settings['page-distributors-locations-display']:'no'),
+            'toggles'=>array(
+                array('value'=>'no', 'label'=>'No'),
+                array('value'=>'wordlist', 'label'=>'List'),
+                array('value'=>'wordcloud', 'label'=>'Cloud'),
+                ),
+            ),
+        array('label'=>'Listing Content',
+            'setting'=>'page-distributors-list-format', 
+            'type'=>'select',
+            'value'=>(isset($settings['page-distributors-list-format'])?$settings['page-distributors-list-format']:'no'),
+            'options'=>array(
+                array('value'=>'shortbio', 'label'=>'Short Bio'),
+                array('value'=>'shortbio-blank-addressesnl-phones-emails-links', 'label'=>'Short Bio, Addresses, Phones, Emails, Links'),
+                array('value'=>'addressesnl-blank-shortbio-phones-emails-links', 'label'=>'Addresses, Short Bio, Phones, Emails, Links'),
+                ),
+            ),
+        ));
+
+	return array('stat'=>'ok', 'pages'=>$pages);
+}
+?>
---
+++ web/processRequest.php
@@ -0,0 +1,29 @@
+<?php
+//
+// Description
+// -----------
+//
+// Arguments
+// ---------
+//
+// Returns
+// -------
+//
+function ciniki_customers_web_processRequest(&$ciniki, $settings, $business_id, $args) {
+
+    if( !isset($ciniki['business']['modules']['ciniki.customers']) ) {
+        return array('stat'=>'404', 'err'=>array('pkg'=>'ciniki', 'code'=>'3050', 'msg'=>"I'm sorry, the page you requested does not exist."));
+    }
+
+
+    if( isset($args['module_page']) && $args['module_page'] == 'ciniki.customers.dealers' ) {
+        ciniki_core_loadMethod($ciniki, 'ciniki', 'customers', 'web', 'processRequestDealers');
+        return ciniki_customers_web_processRequestDealers($ciniki, $settings, $business_id, $args);
+    } elseif( isset($args['module_page']) && $args['module_page'] == 'ciniki.customers.distributors' ) {
+        ciniki_core_loadMethod($ciniki, 'ciniki', 'customers', 'web', 'processRequestDistributors');
+        return ciniki_customers_web_processRequestDistributors($ciniki, $settings, $business_id, $args);
+    }
+
+    return array('stat'=>'404', 'err'=>array('pkg'=>'ciniki', 'code'=>'3051', 'msg'=>"I'm sorry, the page you requested does not exist."));
+}
+?>
