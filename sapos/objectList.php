e06507faaec952ab93aaaa21c8934b81f3d21f90
Andrew Rivett <veggiefrog@gmail.com>
2017-10-24 23:03:58 -0400

    Added hooks for sapos objectList.

---
+++ sapos/objectList.php
@@ -0,0 +1,27 @@
+<?php
+//
+// Description
+// ===========
+// This method returns the list of objects that can be returned
+// as invoice items.
+//
+// Arguments
+// =========
+// 
+// Returns
+// =======
+//
+function ciniki_customers_sapos_objectList($ciniki, $business_id) {
+
+    $objects = array(
+        //
+        // this object should only be added to carts
+        //
+        'ciniki.customers.membership' => array(
+            'name' => 'Membership',
+            ),
+        );
+
+    return array('stat'=>'ok', 'objects'=>$objects);
+}
+?>
