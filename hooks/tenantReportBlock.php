7eea4eebc8e841aad5123cb41d73cc0f311317a6
Andrew Rivett <veggiefrog@gmail.com>
2017-12-04 00:59:17 -0500

    Converted business to tenant

---
+++ hooks/tenantReportBlock.php
@@ -0,0 +1,53 @@
+<?php
+//
+// Description
+// -----------
+// This function will return the report details for a requested report block.
+//
+// Arguments
+// ---------
+// ciniki:
+// tnid:     The ID of the tenant.
+// args:            The possible arguments for.
+//
+//
+// Returns
+// -------
+//
+function ciniki_customers_hooks_tenantReportBlock(&$ciniki, $tnid, $args) {
+
+    //
+    // Check to make sure the module is enabled
+    //
+    if( !isset($ciniki['tenant']['modules']['ciniki.customers']) ) {
+        return array('stat'=>'fail', 'err'=>array('code'=>'ciniki.customers.221', 'msg'=>"That report is not available."));
+    }
+
+    //
+    // Check to make sure the report is specified
+    //
+    if( !isset($args['block_ref']) || !isset($args['options']) ) {
+        return array('stat'=>'fail', 'err'=>array('code'=>'ciniki.customers.222', 'msg'=>"No block specified."));
+    }
+
+    //
+    // The array to store the report data
+    //
+
+    //
+    // Return the list of reports for the tenant
+    //
+    if( $args['block_ref'] == 'ciniki.customers.birthdays' ) {
+        ciniki_core_loadMethod($ciniki, 'ciniki', 'customers', 'private', 'reportUpcomingBirthdays');
+        return ciniki_customers_reportUpcomingBirthdays($ciniki, $tnid, $args['options']);
+    } elseif( $args['block_ref'] == 'ciniki.customers.newcustomers' ) {
+        ciniki_core_loadMethod($ciniki, 'ciniki', 'customers', 'private', 'reportNewCustomers');
+        return ciniki_customers_reportNewCustomers($ciniki, $tnid, $args['options']);
+    } elseif( $args['block_ref'] == 'ciniki.customers.newmembers' ) {
+        ciniki_core_loadMethod($ciniki, 'ciniki', 'customers', 'private', 'reportNewMembers');
+        return ciniki_customers_reportNewMembers($ciniki, $tnid, $args['options']);
+    }
+
+    return array('stat'=>'ok');
+}
+?>
