ad1cb6c656cda27c4972d5bf287b73a94a9451bd
Andrew Rivett <veggiefrog@gmail.com>
2021-03-18 20:15:53 -0400

    Fixed bug when add membership purchase not updating last_paid and expires

--- public/purchaseAdd.php
+++ public/purchaseAdd.php
@@ -45,6 +45,53 @@ function ciniki_customers_purchaseAdd(&$ciniki) {
     }
 
     //
+    // Load product
+    //
+    $strsql = "SELECT products.id, "
+        . "products.code, "
+        . "products.name, "
+        . "products.type, "
+        . "products.status, "
+        . "products.flags, "
+        . "products.months "
+        . "FROM ciniki_customer_products AS products "
+        . "WHERE products.id = '" . ciniki_core_dbQuote($ciniki, $args['product_id']) . "' "
+        . "AND products.tnid = '" . ciniki_core_dbQuote($ciniki, $args['tnid']) . "' "
+        . "";
+    $rc = ciniki_core_dbHashQuery($ciniki, $strsql, 'ciniki.customers', 'product');
+    if( $rc['stat'] != 'ok' ) {
+        return array('stat'=>'fail', 'err'=>array('code'=>'ciniki.customers.419', 'msg'=>'Unable to load product', 'err'=>$rc['err']));
+    }
+    if( !isset($rc['product']) ) {
+        return array('stat'=>'fail', 'err'=>array('code'=>'ciniki.customers.420', 'msg'=>'Unable to find requested product'));
+    }
+    $product = $rc['product'];
+
+    //
+    // Load the customer details
+    //
+    $strsql = "SELECT id, "
+        . "type, "
+        . "parent_id, "
+        . "status, "
+        . "member_status, "
+        . "member_lastpaid, "
+        . "member_expires, "
+        . "membership_length "
+        . "FROM ciniki_customers "
+        . "WHERE id = '" . ciniki_core_dbQuote($ciniki, $args['customer_id']) . "' "
+        . "AND tnid = '" . ciniki_core_dbQuote($ciniki, $args['tnid']) . "' "
+        . "";
+    $rc = ciniki_core_dbHashQuery($ciniki, $strsql, 'ciniki.customers', 'customer');
+    if( $rc['stat'] != 'ok' ) {
+        return array('stat'=>'fail', 'err'=>array('code'=>'ciniki.customers.422', 'msg'=>'Unable to load customer', 'err'=>$rc['err']));
+    }
+    if( !isset($rc['customer']) ) {
+        return array('stat'=>'fail', 'err'=>array('code'=>'ciniki.customers.423', 'msg'=>'Unable to find customer'));
+    }
+    $customer = $rc['customer'];
+
+    //
     // Start transaction
     //
     ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbTransactionStart');
@@ -66,6 +113,50 @@ function ciniki_customers_purchaseAdd(&$ciniki) {
         return $rc;
     }
     $purchase_id = $rc['id'];
+
+    //
+    // Update customer record with membership details
+    //
+    $customer_updates = array();
+    if( $product['type'] == 10 || $product['type'] == 20 ) {
+        //
+        // If new membership, update customer record
+        //
+        if( $args['purchase_date'] > $customer['purchase_date'] ) {
+            if( $customer['status'] != 10 ) {
+                $customer_updates['status'] = 10;
+            }
+            if( $customer['member_status'] != 10 ) {
+                $customer_updates['member_status'] = 10;
+            }
+            $customer_updates['member_lastpaid'] = $args['purchase_date'];
+            if( $args['end_date'] > $customer['member_expires'] ) {
+                $customer_updates['member_expires'] = $args['end_date'];
+            }
+            if( $product['type'] == 20 ) {
+                $customer_updates['membership_length'] = 60;
+            } else {
+                if( $product['months'] == 1 ) {
+                    $customer_updates['membership_length'] = 10;
+                } else {
+                    // Assume yearly, but this is not accurate when dealing with Customer Products for memberships
+                    $customer_updates['membership_length'] = 20;
+                }
+            }
+            $customer_updates['membership_type'] = 200;
+        }
+    } 
+
+    if( count($customer_updates) > 0 ) {
+        //
+        // Update the customer records with new membership details
+        //
+        ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'objectUpdate');
+        $rc = ciniki_core_objectUpdate($ciniki, $args['tnid'], 'ciniki.customers.customer', $args['customer_id'], $customer_updates, 0x04);
+        if( $rc['stat'] != 'ok' ) {
+            return array('stat'=>'fail', 'err'=>array('code'=>'ciniki.customers.424', 'msg'=>'Unable to update the customer'));
+        }
+    }
 
     //
     // Commit the transaction
