e2cefcc5f2085132f1f7facf5eb04ec0132d344d
Andrew Rivett <veggiefrog@gmail.com>
2021-10-13 15:06:40 -0400

    Added form hooks

---
+++ hooks/formDefaultsLoad.php
@@ -0,0 +1,115 @@
+<?php
+//
+// Description
+// -----------
+// Return the list of available field refs for ciniki.forms module.
+// 
+// Arguments
+// ---------
+// ciniki: 
+// tnid:            The ID of the current tenant.
+// 
+// Returns
+// ---------
+// 
+function ciniki_customers_hooks_formDefaultsLoad(&$ciniki, $tnid, $args) {
+   
+    if( !isset($args['form']) ) {
+        return array('stat'=>'ok');
+    }
+    $form = $args['form'];
+   
+    //
+    // Process the customer if specified
+    //
+    if( isset($form['customer_id']) && $form['customer_id'] != '' && $form['customer_id'] > 0 ) {
+        //
+        // Load the customer
+        //
+        ciniki_core_loadMethod($ciniki, 'ciniki', 'customers', 'private', 'customerLoad');
+        $rc = ciniki_customers_customerLoad($ciniki, $tnid, $form['customer_id']);
+        if( $rc['stat'] != 'ok' ) {
+            return array('stat'=>'fail', 'err'=>array('code'=>'ciniki.customers.521', 'msg'=>'Unable to load customer', 'err'=>$rc['err']));
+        }
+        $customer = $rc['customer'];
+
+        //
+        // Load the refs
+        //
+        ciniki_core_loadMethod($ciniki, 'ciniki', 'customers', 'hooks', 'formFieldRefs');
+        $rc = ciniki_customers_hooks_formFieldRefs($ciniki, $tnid, array());
+        if( $rc['stat'] != 'ok' ) {
+            return $rc;
+        }
+        $refs = $rc['refs'];        
+
+        //
+        // Process customer data into refs
+        //
+        if( isset($customer['emails']) ) {
+            if( isset($customer['emails'][0]['id']) ) {
+                $customer['primary_email_id'] = $customer['emails'][0]['id'];
+                $customer['primary_email'] = $customer['emails'][0]['address'];
+            }
+            if( isset($customer['emails'][1]['id']) ) {
+                $customer['secondary_email_id'] = $customer['emails'][1]['id'];
+                $customer['secondary_email'] = $customer['emails'][1]['address'];
+            }
+            // Ignore other emails
+        }
+        if( isset($customer['phones']) ) {
+            foreach($customer['phones'] as $phone) {
+                if( $phone['phone_label'] == 'Cell' && $phone['phone_number'] != '' ) {
+                    $customer['phone_cell_id'] = $phone['id'];
+                    $customer['phone_cell'] = $phone['phone_number'];
+                } elseif( $phone['phone_label'] == 'Home' && $phone['phone_number'] != '' ) {
+                    $customer['phone_home_id'] = $phone['id'];
+                    $customer['phone_home'] = $phone['phone_number'];
+                } elseif( $phone['phone_label'] == 'Work' && $phone['phone_number'] != '' ) {
+                    $customer['phone_work_id'] = $phone['id'];
+                    $customer['phone_work'] = $phone['phone_number'];
+                } elseif( $phone['phone_label'] == 'Fax' && $phone['phone_number'] != '' ) {
+                    $customer['phone_fax_id'] = $phone['id'];
+                    $customer['phone_fax'] = $phone['phone_number'];
+                }
+                // Ignore other phones
+            }
+        }
+        if( isset($customer['addresses']) ) {
+            foreach($customer['addresses'] as $address) {
+                if( ($address['flags']&0x01) == 0x01 && !isset($customer['shipping_address']) ) {
+                     $customer['shipping_address'] = $address;
+                } 
+                if( ($address['flags']&0x02) == 0x02 && !isset($customer['billing_address']) ) {
+                     $customer['billing_address'] = $address;
+                } 
+                if( ($address['flags']&0x04) == 0x04 && !isset($customer['mailing_address']) ) {
+                     $customer['mailing_address'] = $address;
+                }
+            }
+        }
+    
+        if( isset($form['sections']) ) {
+            foreach($form['sections'] as $sid => $section) {
+                if( isset($section['fields']) ) {
+                    foreach($section['fields'] as $fid => $field) {
+                        //
+                        // Check if field ref is for customer and if the reference exists
+                        //
+                        if( isset($field['field_ref']) && $field['field_ref'] != '' 
+                            && preg_match("/^ciniki\.customers\.customer\.([^\.]+)/", $field['field_ref'], $m)
+                            && isset($refs[$field['field_ref']])
+                            ) {
+                            if( isset($customer[$m[1]]) ) {
+                                $form['sections'][$sid]['fields'][$fid]['default'] = $customer[$m[1]];
+                            }
+                        }
+                    }
+                }
+            }
+        } 
+    }
+
+    return array('stat'=>'ok', 'form'=>$form);
+}
+?>
---
+++ hooks/formFieldRefs.php
@@ -0,0 +1,52 @@
+<?php
+//
+// Description
+// -----------
+// Return the list of available field refs for ciniki.forms module.
+// 
+// Arguments
+// ---------
+// ciniki: 
+// tnid:            The ID of the current tenant.
+// 
+// Returns
+// ---------
+// 
+function ciniki_customers_hooks_formFieldRefs(&$ciniki, $tnid, $args) {
+    
+    $module = 'Customers';
+    $refs = array(
+        'ciniki.customers.customer.first' => array('module'=>$module, 'type'=>'text', 'name'=>'First Name'),
+        'ciniki.customers.customer.middle' => array('module'=>$module, 'type'=>'text', 'name'=>'Middle Name'),
+        'ciniki.customers.customer.last' => array('module'=>$module, 'type'=>'text', 'name'=>'Last Name'),
+        'ciniki.customers.customer.company' => array('module'=>$module, 'type'=>'text', 'name'=>'Business'),
+        'ciniki.customers.customer.phone_cell' => array('module'=>$module, 'type'=>'phone', 'name'=>'Cell Phone'),
+        'ciniki.customers.customer.phone_home' => array('module'=>$module, 'type'=>'phone', 'name'=>'Home Phone'),
+        'ciniki.customers.customer.phone_work' => array('module'=>$module, 'type'=>'phone', 'name'=>'Work Phone'),
+        'ciniki.customers.customer.phone_fax' => array('module'=>$module, 'type'=>'phone', 'name'=>'Fax'),
+        'ciniki.customers.customer.primary_email' => array('module'=>$module, 'type'=>'email', 'name'=>'Primary Email'),
+        'ciniki.customers.customer.secondary_email' => array('module'=>$module, 'type'=>'email', 'name'=>'Secondary Email'),
+        'ciniki.customers.customer.mailing_address' => array('module'=>$module, 'type'=>'address', 'name'=>'Mailing Address'),
+        'ciniki.customers.customer.shipping_address' => array('module'=>$module, 'type'=>'address', 'name'=>'Shipping Address'),
+
+/*        'ciniki.customers.customer.mailing_address1' => array('module'=>$module, 'type'=>'text', 'name'=>'Mailing Address Line 1'),
+        'ciniki.customers.customer.mailing_address2' => array('module'=>$module, 'type'=>'text', 'name'=>'Mailing Address Line 2'),
+        'ciniki.customers.customer.mailing_city' => array('module'=>$module, 'type'=>'text', 'name'=>'Mailing City'),
+        'ciniki.customers.customer.mailing_province' => array('module'=>$module, 'type'=>'text', 'name'=>'Mailing Province'),
+        'ciniki.customers.customer.mailing_postal' => array('module'=>$module, 'type'=>'text', 'name'=>'Mailing Postal'),
+        'ciniki.customers.customer.mailing_country' => array('module'=>$module, 'type'=>'text', 'name'=>'Mailing Country'),
+        'ciniki.customers.customer.shipping_address1' => array('module'=>$module, 'type'=>'text', 'name'=>'Shipping Address Line 1'),
+        'ciniki.customers.customer.shipping_address2' => array('module'=>$module, 'type'=>'text', 'name'=>'Shipping Address Line 2'),
+        'ciniki.customers.customer.shipping_city' => array('module'=>$module, 'type'=>'text', 'name'=>'Shipping City'),
+        'ciniki.customers.customer.shipping_province' => array('module'=>$module, 'type'=>'text', 'name'=>'Shipping Province'),
+        'ciniki.customers.customer.shipping_postal' => array('module'=>$module, 'type'=>'text', 'name'=>'Shipping Postal'),
+        'ciniki.customers.customer.shipping_country' => array('module'=>$module, 'type'=>'text', 'name'=>'Shipping Country'),
+        */
+        'ciniki.customers.customer.website' => array('module'=>$module, 'type'=>'url', 'name'=>'Website'),
+        'ciniki.customers.customer.facebook' => array('module'=>$module, 'type'=>'url', 'name'=>'Facebook'),
+        'ciniki.customers.customer.instagram' => array('module'=>$module, 'type'=>'url', 'name'=>'Instagram'),
+        );
+
+    return array('stat'=>'ok', 'refs'=>$refs);
+}
+?>
