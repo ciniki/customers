e684e02553742046bd3dfa6af5c22a1f3c585fbe
Andrew Rivett <veggiefrog@gmail.com>
2025-02-18 19:37:04 -0500

    Fixed UI bug in members

--- ui/memberships.js
+++ ui/memberships.js
@@ -479,9 +479,9 @@ function ciniki_customers_memberships() {
             this.member.sections.membership.list.member_lastpaid.visible = 'yes';
             this.member.sections.membership.list.member_expires.visible = 'yes';
             this.member.sections.membership.list.type.visible = 'yes';
-            this.menu.sections.members.num_cols = 3;
-            this.menu.sections.members.headerValues = ['Name', 'Paid', 'Expires'];
-            this.menu.sections.members.sortTypes = ['text', 'date', 'date'];
+            this.menu.sections.members.num_cols = 4;
+            this.menu.sections.members.headerValues = ['First', 'Last', 'Paid', 'Expires'];
+            this.menu.sections.members.sortTypes = ['text', 'text', 'date', 'date'];
         } else {
             this.member.sections.membership.list.member_lastpaid.visible = 'no';
             this.member.sections.membership.list.member_expires.visible = 'no';
