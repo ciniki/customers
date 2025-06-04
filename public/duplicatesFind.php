754631148ee0ab2a9dd543db011746ac9fc763fb
Andrew Rivett <veggiefrog@gmail.com>
2025-06-04 10:13:03 -0400

    Added duplicates find based on email

--- public/duplicatesFind.php
+++ public/duplicatesFind.php
@@ -40,36 +40,50 @@ function ciniki_customers_duplicatesFind($ciniki) {
         . "c1.id AS c1_id, "
         . "c1.display_name AS c1_display_name, "
         . "c2.id AS c2_id, "
-        . "c2.display_name AS c2_display_name "
-        . "FROM ciniki_customers AS c1, ciniki_customers AS c2 "
-        . "WHERE c1.tnid = '" . ciniki_core_dbQuote($ciniki, $args['tnid']) . "' "
-        . "AND c2.tnid = '" . ciniki_core_dbQuote($ciniki, $args['tnid']) . "' "
-        . "AND c1.id < c2.id ";
-//      . "AND ((c1.first = c2.first and c1.last = c2.last) OR (c1.first = c2.last and c1.last = c2.first)) "
-    if( isset($args['matchon']) && $args['matchon'] == 'exact' ) {
-        $strsql .= "AND c1.first = c2.first "
-            . "AND c1.last = c2.last "
-            . "";
-    } elseif( isset($args['matchon']) && $args['matchon'] == 'email' ) {
-        $strsql .= "AND c1.first = c2.first "
-            . "AND c1.last = c2.last "
-            . "";
-
+        . "c2.display_name AS c2_display_name ";
+    if( isset($args['matchon']) && $args['matchon'] == 'email' ) {
+        $strsql .= "FROM ciniki_customers AS c1 "
+            . "INNER JOIN ciniki_customer_emails AS e1 ON ("
+                . "c1.id = e1.customer_id "
+                . "AND e1.tnid = '" . ciniki_core_dbQuote($ciniki, $args['tnid']) . "' "
+                . ") "
+            . "INNER JOIN ciniki_customer_emails AS e2 ON ("
+                . "e1.email = e2.email "
+                . "AND e1.email <> '' "
+                . "AND e2.tnid = '" . ciniki_core_dbQuote($ciniki, $args['tnid']) . "' "
+                . ") "
+            . "INNER JOIN ciniki_customers AS c2 ON ("
+                . "e2.customer_id = c2.id "
+                . "AND c2.tnid = '" . ciniki_core_dbQuote($ciniki, $args['tnid']) . "' "
+                . ") "
+            . "WHERE c1.tnid = '" . ciniki_core_dbQuote($ciniki, $args['tnid']) . "' "
+            . "AND c1.id < c2.id ";
     } else {
-        $strsql .= "AND ("
-            . "((c1.type = 1 OR c1.type = 10 OR c1.type = 21 OR c1.type = 22 OR c1.type = 31 OR c1.type = 32) "
-                . "AND (c2.type = 1 OR c2.type = 10 OR c1.type = 21 OR c1.type = 22 OR c1.type = 31 OR c1.type = 32)"
-                . "AND ("
-                    . "(c1.first <> '' AND soundex(c1.first) = soundex(c2.first) AND soundex(c1.last) = soundex(c2.last)) "
-                    . "OR (c1.first <> '' AND c1.last <> '' AND soundex(c1.first) = soundex(c2.last) AND soundex(c1.last) = soundex(c2.first)) "
-                . ")) "
-            . "OR ((c1.type = 2 OR c1.type = 20 OR c1.type = 30) AND (c2.type = 2 OR c2.type = 20 OR c2.type = 30) AND soundex(c1.company) = soundex(c2.company)) "
-            . ") ";
-//        . "AND ((c1.type = 1 AND soundex(c1.first) = soundex(c2.first) and soundex(c1.last) = soundex(c2.last)) "
-//            . "OR (c1.type = 1 AND soundex(c1.first) = soundex(c2.last) and soundex(c1.last) = soundex(c2.first)) "
-//            . "OR (c1.type = 2 AND soundex(c1.company) = soundex(c2.company)) "
-//            . "OR (soundex(c1.display_name) = soundex(c2.display_name)) "
-//            . ") "
+        $strsql .= "FROM ciniki_customers AS c1, ciniki_customers AS c2 "
+            . "WHERE c1.tnid = '" . ciniki_core_dbQuote($ciniki, $args['tnid']) . "' "
+            . "AND c2.tnid = '" . ciniki_core_dbQuote($ciniki, $args['tnid']) . "' "
+            . "AND c1.id < c2.id ";
+    //      . "AND ((c1.first = c2.first and c1.last = c2.last) OR (c1.first = c2.last and c1.last = c2.first)) "
+        if( isset($args['matchon']) && $args['matchon'] == 'exact' ) {
+            $strsql .= "AND c1.first = c2.first "
+                . "AND c1.last = c2.last "
+                . "";
+        } else {
+            $strsql .= "AND ("
+                . "((c1.type = 1 OR c1.type = 10 OR c1.type = 21 OR c1.type = 22 OR c1.type = 31 OR c1.type = 32) "
+                    . "AND (c2.type = 1 OR c2.type = 10 OR c1.type = 21 OR c1.type = 22 OR c1.type = 31 OR c1.type = 32)"
+                    . "AND ("
+                        . "(c1.first <> '' AND soundex(c1.first) = soundex(c2.first) AND soundex(c1.last) = soundex(c2.last)) "
+                        . "OR (c1.first <> '' AND c1.last <> '' AND soundex(c1.first) = soundex(c2.last) AND soundex(c1.last) = soundex(c2.first)) "
+                    . ")) "
+                . "OR ((c1.type = 2 OR c1.type = 20 OR c1.type = 30) AND (c2.type = 2 OR c2.type = 20 OR c2.type = 30) AND soundex(c1.company) = soundex(c2.company)) "
+                . ") ";
+    //        . "AND ((c1.type = 1 AND soundex(c1.first) = soundex(c2.first) and soundex(c1.last) = soundex(c2.last)) "
+    //            . "OR (c1.type = 1 AND soundex(c1.first) = soundex(c2.last) and soundex(c1.last) = soundex(c2.first)) "
+    //            . "OR (c1.type = 2 AND soundex(c1.company) = soundex(c2.company)) "
+    //            . "OR (soundex(c1.display_name) = soundex(c2.display_name)) "
+    //            . ") "
+        }
     }
     $strsql .= "ORDER BY c1_display_name, c1.id "
         . "";
